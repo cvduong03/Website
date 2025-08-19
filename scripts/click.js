bebdf9ec92a275d6ea27e45576f0ecc96e977f28
Calvin Duong <calvinduong1337@gmail.com>
2025-08-19 10:32:40 -0700

    Reverted github push because it duplicated code

--- scripts/click.js
+++ scripts/click.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 function hamburgerTransition(x) {
   // animate toggle to X
   x.classList.toggle('change');
@@ -6,12 +5,3 @@ function hamburgerTransition(x) {
   const dropDown = document.getElementById("dropdownMenu");
   dropDown.classList.toggle("active");
 }
-=======
-function hamburgerTransition(x) {
-  // animate toggle to X
-  x.classList.toggle('change');
-
-  const dropDown = document.getElementById("dropdownMenu");
-  dropDown.classList.toggle("active");
-}
->>>>>>> 398f153bf1e94d531e36fd360d49f42fa1ec0d2e
