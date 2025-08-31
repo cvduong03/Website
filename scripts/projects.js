2fff11fd0437984badf5ca831642e32c46ed5b61
Calvin Duong <calvinduong1337@gmail.com>
2025-08-30 21:09:08 -0700

    fix for github pages

---
+++ scripts/projects.js
@@ -0,0 +1,16 @@
+const projects = [
+  {
+    title: "The Sustainable Smart Home Project",
+    description: "The project demonstrates the benefits of automation logic in smart home appliances to conveniently and efficiently provide common comforts. Combined with a battery powered system supplied by solar energy, the platform can self-sustain and operate for 19 hours on essential loads.",
+    image: "images/projects/smart-home.png",
+    link: "projects/smart-home.html"
+  },
+
+  {
+    title: "The Sustainable Smart Home Project",
+    description: "Built to help my parents guage safe parking distance of their new car in the garage. Equipped with an ultrasonic sensor, the measured distance will illuminate an RGB LED strip from green ",
+    image: "images/projects/garage-parking-sensor.png",
+    link: "projects/garage-parking-sensor.html"
+  }
+
+]
